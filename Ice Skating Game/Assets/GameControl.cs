19f8ba96118aea012a7f485d7fca2a1721173ad3
carlislecalabrese <cs1563@messiah.edu>
2018-06-25 11:58:59 -0400

    Ice Skating Game
    Revised again

---
+++ Ice Skating Game/Assets/GameControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+
+    public GameObject Spawner_1;
+    public GameObject Spawner_2;
+    public float Score;
+    public float Lives;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Ice Skating Game/Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public Rigidbody2D RB;
+    public float JumpSpeed;
+    bool CanJump = true;
+    public GameObject GameController;
+
+
+
+    // Update is called once per frame
+    void Update()
+    { 
+        if (Input.GetButtonDown("Jump") && CanJump == true)
+        {
+            RB.AddForce(Vector2.up * JumpSpeed);
+            CanJump = false;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (RB.velocity.y == 0)
+        {
+            CanJump = true;
+        }
+    }
+}
