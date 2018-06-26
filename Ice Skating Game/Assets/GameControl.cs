e9107d15d1e3291b4a46a33d3f0d52e82b19509c
carlislecalabrese <cs1563@messiah.edu>
2018-06-26 13:16:15 -0400

    Ice Skating Game
    Updated Again

--- Ice Skating Game/Assets/GameControl.cs
+++ Ice Skating Game/Assets/GameControl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour {
 
@@ -8,10 +9,14 @@ public class GameControl : MonoBehaviour {
     public GameObject Spawner_2;
     public float Score;
     public float Lives;
+
+    public Text ScoreText;
+    public Text LifeText;
     
 	// Update is called once per frame
 	void Update () {
-        
+        ScoreText.text = "Score: " + Score + " Lives: " + Lives;
+        LifeText.text = "Lives:" + Lives;
     }
 
 
