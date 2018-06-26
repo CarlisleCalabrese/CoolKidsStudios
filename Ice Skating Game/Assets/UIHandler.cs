56b4fd4e434c6bea5fb31e2ebc0f28d884728c9e
carlislecalabrese <cs1563@messiah.edu>
2018-06-26 10:22:39 -0400

    Ice Skating Game
    Added UI That Works

---
+++ Ice Skating Game/Assets/UIHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIhandler : MonoBehaviour {
+
+    public void LoadScene(string SceneToLoad)
+    {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+	
+}
